5ba29487ee2a2e9326df9237d92e772ced3c1c13
not-matthias <26800596+not-matthias@users.noreply.github.com>
2020-09-27 12:27:14 +0200

    cargo clippy

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -7,5 +7,5 @@ pub fn xor<K: AsRef<EncryptionKey>>(data: &mut [u8], key: K) {
     let key = key.as_ref();
 
     data.chunks_mut(key.len())
-        .for_each(|d| d.into_iter().zip(&**key).for_each(|(d, k)| *d ^= *k));
+        .for_each(|d| d.iter_mut().zip(&**key).for_each(|(d, k)| *d ^= *k));
 }
